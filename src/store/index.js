28fa40a8c7c9f38f7edfac047c0ed6f861c05c6c
Nyamper <campersf@gmail.com>
2022-07-11 21:04:45 +0300

    move to rtk

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,38 @@
-import {
-  legacy_createStore as createStore,
-  applyMiddleware,
-  compose,
-} from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 
 import rootSaga from './sagas';
-import rootReducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(sagaMiddleware),
+});
 
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
-
-const store = createStore(rootReducer, enhancer);
 sagaMiddleware.run(rootSaga);
 
 export default store;
+
+// import {
+//   legacy_createStore as createStore,
+//   applyMiddleware,
+//   compose,
+// } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+
+// import rootSaga from './sagas';
+// import rootReducer from './reducers';
+
+// const sagaMiddleware = createSagaMiddleware();
+
+// const composeEnhancers =
+//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
+
+// const enhancer = composapplyMiddlewareSaga);
+
+// export default store;
